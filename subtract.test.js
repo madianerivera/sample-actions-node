127cacb4b72a459950d047b7abdbf1753a61738b
Ma. Diane Rivera <madianerivera@Mas-MacBook-Air.local>
2022-09-01 15:40:32 +0800

    add tests

--- subtract.test.js
+++ subtract.test.js
@@ -3,3 +3,7 @@ const subtract = require('./subtract');
 test('subtract 3 - 2 to equal 1', () => {
   expect(subtract(3, 2)).toBe(1);
 });
+
+test('subtract 3 - 5 to equal -2', () => {
+  expect(subtract(3, 5)).toBe(-2);
+});
