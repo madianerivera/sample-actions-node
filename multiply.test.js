c59b4492cbe25f3c473a51c879d8ce1dc6f04e17
Ma. Diane Rivera <madianerivera@Mas-MacBook-Air.local>
2022-09-02 14:36:44 +0800

    something2

--- multiply.test.js
+++ multiply.test.js
@@ -7,3 +7,7 @@ test('multiply 3 * 2 to equal 6', () => {
 test('multiply 3 * 3 to equal 9', () => {
   expect(multiply(3, 3)).toBe(9);
 });
+
+test('multiply 3 * 1 to equal 3', () => {
+  expect(multiply(3, 1)).toBe(3);
+});
