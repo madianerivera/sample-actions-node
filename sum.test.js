f14524677f55ebb438cb0cf39119936e4e7987ac
Ma. Diane Rivera <madianerivera@Mas-MacBook-Air.local>
2022-09-06 17:29:38 +0800

    fix: testtt

--- sum.test.js
+++ sum.test.js
@@ -11,3 +11,7 @@ test('adds 2 + 2 to equal 4', () => {
 test('adds 2 + 2 to equal 4', () => {
   expect(sum(2, 2)).toBe(4);
 });
+
+test('adds 2 + 2 to equal 4', () => {
+  expect(sum(2, 2)).toBe(4);
+});
