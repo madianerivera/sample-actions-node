5799ef725600294c02afac7c25d581515d47b237
Ma. Diane Rivera <madianerivera@Mas-MacBook-Air.local>
2022-09-07 14:39:30 +0800

    ci: hello

--- subtract.test.js
+++ subtract.test.js
@@ -15,3 +15,7 @@ test('subtract 6 - 2 to equal 4', () => {
 test('subtract 6 - 2 to equal 4', () => {
   expect(subtract(6, 2)).toBe(4);
 });
+
+test('subtract 6 - 2 to equal 4', () => {
+  expect(subtract(6, 2)).toBe(4);
+});
